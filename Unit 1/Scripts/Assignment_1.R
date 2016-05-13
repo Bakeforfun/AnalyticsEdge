bce43216330709173ed96b4c4c6f20b737a6cd60
Vladislav Zakatov <Baker@bakeforfun.local>
2016-05-13 07:51:52 +0300

    Initial commit

---
+++ Unit 1/Scripts/Assignment_1.R
@@ -0,0 +1,203 @@
+setwd("~/Dropbox/Education/edX/The Analytics Edge/Unit 1")
+
+##### AN ANALYTICAL DETECTIVE #####
+### Problem 1 - Loading the Data ###
+mvt = read.csv("Data/mvtWeek1.csv")
+str(mvt)
+summary(mvt)
+
+max(mvt$ID)
+min(mvt$Beat)
+
+table(mvt$Arrest)
+
+nrow(mvt[mvt$LocationDescription == "ALLEY",])
+
+### Problem 2 - Understanding Dates in R ###
+mvt$Date[1]
+DateConvert = as.Date(strptime(mvt$Date, "%m/%d/%y %H:%M"))
+summary(DateConvert)
+
+mvt$Month = months(DateConvert)
+mvt$Weekday = weekdays(DateConvert)
+mvt$Date = DateConvert
+
+which.min(table(mvt$Month))
+which.max(table(mvt$Weekday))
+
+which.max(table(mvt$Month, mvt$Arrest)[,2])
+
+### Problem 3 - Visualizing Crime Trends ###
+hist(mvt$Date, breaks=100)
+boxplot(mvt$Date)
+
+mvt$Year = as.numeric(format(mvt$Date, "%Y")) 
+table(mvt$Year, mvt$Arrest)
+table(mvt$Year, mvt$Arrest)[1,2] / (table(mvt$Year, mvt$Arrest)[1,1] + table(mvt$Year, mvt$Arrest)[1,2])
+table(mvt$Year, mvt$Arrest)[7,2] / (table(mvt$Year, mvt$Arrest)[7,1] + table(mvt$Year, mvt$Arrest)[7,2])
+table(mvt$Year, mvt$Arrest)[12,2] / (table(mvt$Year, mvt$Arrest)[12,1] + table(mvt$Year, mvt$Arrest)[12,2])
+
+### Problem 4 - Popular Locations ###
+sort(table(mvt$LocationDescription))
+
+Top5 = subset(mvt, mvt$LocationDescription == "STREET" |
+                   mvt$LocationDescription == "PARKING LOT/GARAGE(NON.RESID.)" |
+                   mvt$LocationDescription == "ALLEY" |
+                   mvt$LocationDescription == "GAS STATION" |
+                   mvt$LocationDescription == "DRIVEWAY - RESIDENTIAL")
+
+Top5$LocationDescription = factor(Top5$LocationDescription)
+summary(Top5$LocationDescription)
+
+table(Top5$LocationDescription, Top5$Arrest)
+table(Top5$LocationDescription, Top5$Arrest)[, 2] / 
+  (table(Top5$LocationDescription, Top5$Arrest)[, 1] + table(Top5$LocationDescription, Top5$Arrest)[, 2])
+
+
+
+##### STOCK DYNAMICS #####
+### Problem 1 - Summary Statistics ###
+IBM = read.csv("data/IBMStock.csv")
+GE = read.csv("data/GEStock.csv")
+ProcterGamble = read.csv("data/ProcterGambleStock.csv")
+CocaCola = read.csv("data/CocaColaStock.csv")
+Boeing = read.csv("data/BoeingStock.csv")
+
+IBM$Date = as.Date(IBM$Date, "%m/%d/%y")
+GE$Date = as.Date(GE$Date, "%m/%d/%y")
+CocaCola$Date = as.Date(CocaCola$Date, "%m/%d/%y")
+ProcterGamble$Date = as.Date(ProcterGamble$Date, "%m/%d/%y")
+Boeing$Date = as.Date(Boeing$Date, "%m/%d/%y")
+
+summary(IBM$Date)
+mean(IBM$StockPrice)
+min(GE$StockPrice)
+max(CocaCola$StockPrice)
+median(Boeing$StockPrice)
+sd(ProcterGamble$StockPrice)
+
+### Problem 2 - Visualizing Stock Dynamics ###
+plot(CocaCola, type='l', main = 'Coca-Cola and IBM stock prices', ylab = '', xlab = 'Date', col="red")
+lines(ProcterGamble$Date, ProcterGamble$StockPrice, col="blue")
+abline(v=as.Date(c("2000-03-01")), lwd=2)
+
+plot(CocaCola, type='l', main = 'Coca-Cola and IBM stock prices', ylab = '', xlab = 'Date', col="red")
+lines(ProcterGamble$Date, ProcterGamble$StockPrice, col="blue")
+abline(v=as.Date(c("1983-03-01")), lwd=2)
+
+### Problem 3 - Visualizing Stock Dynamics 1995-2005 ###
+plot(CocaCola$Date[301:432], CocaCola$StockPrice[301:432], type="l", col="red", ylim=c(0,210),
+     main = 'Stock prices', ylab = '', xlab = 'Date')
+lines(IBM$Date[301:432], IBM$StockPrice[301:432], col="blue")
+lines(GE$Date[301:432], GE$StockPrice[301:432], col="green")
+lines(ProcterGamble$Date[301:432], ProcterGamble$StockPrice[301:432], col="purple")
+lines(Boeing$Date[301:432], Boeing$StockPrice[301:432], col="orange")
+abline(v=as.Date(c("1997-09-01")), lwd=2)
+abline(v=as.Date(c("1997-11-01")), lwd=2)
+
+sort(tapply(IBM$StockPrice, months(IBM$Date), mean))
+mean(IBM$StockPrice)
+
+sort(tapply(GE$StockPrice, months(GE$Date), mean))
+sort(tapply(CocaCola$StockPrice, months(CocaCola$Date), mean))
+
+##### DEMOGRAPHICS AND EMPLOYMENT IN THE UNITED STATES #####
+### Problem 1 - Loading and Summarizing the Dataset ###
+CPS = read.csv('data/CPSData.csv')
+str(CPS)
+summary(CPS)
+
+sort(table(CPS$State)) 
+
+(table(CPS$Citizenship)[1] + table(CPS$Citizenship)[2]) / sum(table(CPS$Citizenship))
+
+table(CPS$Race, CPS$Hispanic)
+
+### Problem 2 - Evaluating Missing Values ###
+table(CPS$Region, is.na(CPS$Married))
+table(CPS$Sex, is.na(CPS$Married))
+table(CPS$Age, is.na(CPS$Married))
+table(CPS$Citizenship, is.na(CPS$Married))
+
+table(CPS$State, is.na(CPS$MetroAreaCode))
+
+ftable(CPS$Region, is.na(CPS$MetroAreaCode))
+
+sort(tapply(is.na(CPS$MetroAreaCode), CPS$State, mean))
+
+### Problem 3 - Integrating Metropolitan Area Data ###
+MetroAreaMap = read.csv('data/MetroAreaCodes.csv')
+CountryMap = read.csv('data/CountryCodes.csv')
+str(MetroAreaMap)
+summary(MetroAreaMap)
+str(CountryMap)
+summary(CountryMap)
+
+CPS = merge(CPS, MetroAreaMap, by.x="MetroAreaCode", by.y="Code", all.x=TRUE)
+sum(is.na(CPS$MetroArea))
+
+sort(table(CPS$MetroArea))
+
+sort(tapply(CPS$Hispanic, CPS$MetroArea, mean))
+sort(tapply(CPS$Race == 'Asian', CPS$MetroArea, mean))
+
+sort(tapply(CPS$Education == "No high school diploma", CPS$MetroArea, mean, na.rm = TRUE))
+
+### Problem 4 - Integrating Country of Birth Data ###
+CPS = merge(CPS, CountryMap, by.x="CountryOfBirthCode", by.y="Code", all.x=TRUE)
+sum(is.na(CPS$Country))
+
+sort(table(CPS$Country))
+
+tapply(CPS$Country != 'United States', CPS$MetroArea, mean, na.rm = TRUE)
+
+sort(tapply(CPS$Country == 'India', CPS$MetroArea, sum, na.rm = TRUE))
+sort(tapply(CPS$Country == 'Brazil', CPS$MetroArea, sum, na.rm = TRUE))
+sort(tapply(CPS$Country == 'Somalia', CPS$MetroArea, sum, na.rm = TRUE))
+
+##### INTERNET PRIVACY POLL #####
+### Problem 1 - Loading and Summarizing the Dataset ###
+poll = read.csv('data/AnonymityPoll.csv')
+str(poll)
+summary(poll)
+
+sum(poll$Smartphone, na.rm = TRUE)
+sum(!poll$Smartphone, na.rm = TRUE)
+sum(is.na(poll$Smartphone))
+
+table(poll$State, poll$Region)
+
+### Problem 2 - Internet and Smartphone Users ###
+sum(!poll$Internet.Use & !poll$Smartphone, na.rm = TRUE)
+sum(poll$Internet.Use & poll$Smartphone, na.rm = TRUE)
+sum(poll$Internet.Use & !poll$Smartphone, na.rm = TRUE)
+sum(!poll$Internet.Use & poll$Smartphone, na.rm = TRUE)
+
+limited = subset(poll, poll$Internet.Use | poll$Smartphone)
+
+### Problem 3 - Summarizing Opinions about Internet Privacy ###
+str(limited)
+summary(limited)
+
+mean(limited$Info.On.Internet)
+
+sum(limited$Info.On.Internet == 0)
+sum(limited$Info.On.Internet == 11)
+
+mean(limited$Worry.About.Info, na.rm = TRUE)
+mean(limited$Anonymity.Possible, na.rm = TRUE)
+mean(limited$Tried.Masking.Identity, na.rm = TRUE)
+mean(limited$Privacy.Laws.Effective, na.rm = TRUE)
+
+### Problem 4 - Relating Demographics to Polling Results ###
+hist(limited$Age)
+
+plot(limited$Age, limited$Info.On.Internet)
+table(limited$Age, limited$Info.On.Internet)
+max(table(limited$Age, limited$Info.On.Internet))
+
+plot(jitter(limited$Age), jitter(limited$Info.On.Internet))
+plot(jitter(limited$Age), (limited$Info.On.Internet))
+
+tapply(limited$Info.On.Internet, limited$Smartphone, mean)
+tapply(limited$Tried.Masking.Identity, limited$Smartphone, mean, na.rm = TRUE)
---
+++ Unit 2/Scripts/Assignment_2.R
@@ -0,0 +1,226 @@
+setwd("~/Dropbox/Education/edX/The Analytics Edge/Unit 2")
+
+##### CLIMATE CHANGE #####
+### Problem 1 - Creating Our First Model ###
+clmt = read.csv("Data/climate_change.csv")
+clmt_train = subset(clmt, clmt$Year <= 2006)
+clmt_test = subset(clmt, clmt$Year > 2006)
+
+lm1 = lm(Temp ~ MEI + CO2 + CH4 + N2O + CFC.11 + CFC.12 + TSI + Aerosols, data = clmt_train)
+summary(lm1)
+
+### Problem 2 - Understanding the Model ###
+cor(clmt_train)
+
+### Problem 3 - Simplifying the Model ###
+lm2 = lm(Temp ~ MEI + N2O + TSI + Aerosols, data = clmt_train)
+summary(lm2)
+
+### Problem 4 - Automatically Building the Model ###
+lm3 = step(lm1)
+summary(lm3)
+
+### Problem 5 - Testing on Unseen Data ###
+pred = predict(lm3, newdata = clmt_test)
+SSE = sum((clmt_test$Temp - pred)^2) # RSS
+SST = sum((clmt_test$Temp - mean(clmt_train$Temp))^2) # mean calculated on train set --> baseline model
+R2 = 1 - SSE/SST
+
+##### READING TEST SCORES #####
+### Problem 1.1 - Dataset size ###
+pisaTrain = read.csv("Data/pisa2009train.csv")
+pisaTest = read.csv("Data/pisa2009test.csv")
+str(pisaTrain)
+
+### Problem 1.2 - Summarizing the dataset ###
+tapply(pisaTrain$readingScore, pisaTrain$male, mean)
+
+### Problem 1.3 - Locating missing values ###
+summary(pisaTrain)
+
+### Problem 1.4 - Removing missing values ###
+pisaTrain = na.omit(pisaTrain)
+pisaTest = na.omit(pisaTest)
+str(pisaTrain)
+str(pisaTest)
+
+### Problem 2.1 - Factor variables ###
+str(as.factor(pisaTrain$grade))
+str(as.factor(pisaTrain$male))
+str(as.factor(pisaTrain$raceeth))
+
+### Problem 2.2 - Unordered factors in regression models ###
+summary(as.factor(pisaTrain$raceeth))
+
+### Problem 3.1 - Building a model ###
+pisaTrain$raceeth = relevel(pisaTrain$raceeth, "White")
+pisaTest$raceeth = relevel(pisaTest$raceeth, "White")
+
+lmScore = lm(readingScore ~ ., data = pisaTrain)
+summary(lmScore)
+
+### Problem 3.2 - Computing the root-mean squared error of the model ###
+SSE = sum((lmScore$residuals)^2)
+RMSE = sqrt(SSE / nrow(pisaTrain))
+
+### Problem 3.3 - Comparing predictions for similar students ###
+# 2 * 29.542707 = 59.09
+
+### Problem 4.1 - Predicting on unseen data ###
+pred = predict(lmScore, newdata = pisaTest)
+summary(pred)
+range = max(pred) - min(pred)
+
+### Problem 4.2 - Test set SSE and RMSE ###
+SSE = sum((pisaTest$readingScore - pred)^2)
+RMSE = sqrt(SSE / nrow(pisaTest))
+
+### Problem 4.3 - Baseline prediction and test-set SSE ###
+baseline = mean(pisaTrain$readingScore)
+SST = sum((pisaTest$readingScore - baseline)^2)
+
+### Problem 4.4 - Test-set R-squared ###
+R2 = 1 - SSE/SST
+
+##### DETECTING FLU EPIDEMICS VIA SEARCH ENGINE QUERY DATA #####
+### Problem 1 - Understanding the Data ###
+FluTrain = read.csv("Data/FluTrain.csv")
+subset(FluTrain, ILI == max(ILI))
+FluTrain$Week[which.max(FluTrain$ILI)]
+
+subset(FluTrain, Queries == max(Queries))
+
+hist(FluTrain$ILI)
+plot(FluTrain$Queries, log(FluTrain$ILI))
+
+### Problem 2 - Linear Regression Model ###
+FluTrend1 = lm(log(ILI) ~ Queries, data = FluTrain)
+summary(FluTrend1)
+
+r = cor(FluTrain$Queries, log(FluTrain$ILI))
+r^2
+log(1/r)
+exp(-0.5*r)
+
+### Problem 3.1 - Performance on the Test Set ###
+FluTest = read.csv("Data/FluTest.csv")
+PredTest1 = exp(predict(FluTrend1, newdata=FluTest))
+
+FluTest[which(FluTest$Week == "2012-03-11 - 2012-03-17"), ]
+PredTest1[which(FluTest$Week == "2012-03-11 - 2012-03-17")]
+
+relativeError = (FluTest$ILI - PredTest1)/FluTest$ILI
+relativeError[which(FluTest$Week == "2012-03-11 - 2012-03-17")]
+
+SSE = sum((FluTest$ILI - PredTest1)^2)
+RMSE = sqrt(SSE / nrow(FluTest))
+
+### Problem 4 - Training a Time Series Model ###
+library(zoo)
+
+ILILag2 = lag(zoo(FluTrain$ILI), -2, na.pad=TRUE)
+FluTrain$ILILag2 = coredata(ILILag2)
+summary(FluTrain)
+
+plot(log(FluTrain$ILI), log(FluTrain$ILILag2))
+
+FluTrend2 = lm(log(ILI) ~ Queries + log(ILILag2), data = FluTrain)
+summary(FluTrend2)
+
+### Problem 5.1 - Evaluating the Time Series Model in the Test Set ###
+ILILag2 = lag(zoo(FluTest$ILI), -2, na.pad=TRUE)
+FluTest$ILILag2 = coredata(ILILag2)
+summary(FluTest$ILILag2)
+
+FluTest$ILILag2[1] = FluTrain$ILI[416]
+FluTest$ILILag2[2] = FluTrain$ILI[417]
+
+PredTest2 = exp(predict(FluTrend2, newdata=FluTest))
+SSE = sum((FluTest$ILI - PredTest2)^2)
+RMSE = sqrt(SSE / nrow(FluTest))
+
+##### STATE DATA (OPTIONAL) #####
+### Problem 1 - Data Exploration ###
+data(state)
+
+statedata = cbind(data.frame(state.x77), state.abb, state.area, state.center,
+                  state.division, state.name, state.region)
+
+plot(statedata$x, statedata$y)
+
+tapply(statedata$HS.Grad, statedata$state.region, mean)
+
+plot(statedata$state.region, statedata$Murder)
+
+NortheastData = subset(statedata, state.region == "Northeast")
+NortheastData$state.abb[which.max(NortheastData$Murder)]
+
+### Problem 2 - Predicting Life Expectancy - An Initial Model ###
+lm1 = lm(Life.Exp ~ Population + Income + Illiteracy + Murder + HS.Grad + Frost + Area, data = statedata)
+summary(lm1)
+
+plot(statedata$Income, statedata$Life.Exp)
+
+### Problem 3 - Predicting Life Expectancy - Refining the Model and Analyzing Predictions ###
+lm2 = lm(Life.Exp ~ Population + Income + Illiteracy + Murder + HS.Grad + Frost, data = statedata)
+summary(lm2)
+
+lm3 = lm(Life.Exp ~ Population + Income + Murder + HS.Grad + Frost, data = statedata)
+summary(lm3)
+
+lm4 = lm(Life.Exp ~ Population + Murder + HS.Grad + Frost, data = statedata)
+summary(lm4)
+
+predTrain = predict(lm4)
+sort(predTrain)
+statedata$state.name[which.min(statedata$Life.Exp)]
+sort(predTrain)
+statedata$state.name[which.max(statedata$Life.Exp)]
+
+sort(abs(lm4$residuals))
+# sort(abs(statedata$Life.Exp - predict(lm4)))
+
+##### FORECASTING ELANTRA SALES (OPTIONAL) #####
+### Problem 1 - Loading the Data ###
+elantra = read.csv("Data/elantra.csv")
+elantraTrain = subset(elantra, elantra$Year <= 2012)
+elantraTest = subset(elantra, elantra$Year > 2012)
+
+### Problem 2 - A Linear Regression Model ###
+lm1 = lm(ElantraSales ~ Unemployment + CPI_all + CPI_energy + Queries, data = elantraTrain)
+summary(lm1)
+
+### Problem 3 - Modeling Seasonality ###
+lm2 = lm(ElantraSales ~ Month + Unemployment + CPI_all + CPI_energy + Queries, data = elantraTrain)
+summary(lm2)
+
+### Problem 4 - A New Model ###
+elantraTrain$Month = as.factor(elantraTrain$Month)
+elantraTest$Month = as.factor(elantraTest$Month)
+
+lm3 = lm(ElantraSales ~ Month + Unemployment + CPI_all + CPI_energy + Queries, data = elantraTrain)
+summary(lm3)
+
+### Problem 5 - Multicolinearity ###
+elantraTrain$Month = as.numeric(elantraTrain$Month)
+cor(elantraTrain[c("Unemployment","Month","Queries","CPI_energy","CPI_all")])
+elantraTrain$Month = as.factor(elantraTrain$Month)
+
+### Problem 6.1 - A Reduced Model ###
+lm3 = lm(ElantraSales ~ Month + Unemployment + CPI_all + CPI_energy + Queries, data = elantraTrain)
+summary(lm3)
+
+lm4 = lm(ElantraSales ~ Month + Unemployment + CPI_all + CPI_energy, data = elantraTrain)
+summary(lm4)
+
+### Problem 6.2 - Test Set Predictions ###
+pred = predict(lm4, newdata = elantraTest)
+SSE = sum((elantraTest$ElantraSales - pred)^2)
+baseline = mean(elantraTrain$ElantraSales)
+SST = sum((elantraTest$ElantraSales - baseline)^2)
+R2 = 1 - SSE/SST
+
+errors = elantraTest$ElantraSales - pred
+sort(abs(errors))
+
+elantraTest[which.max(errors), ]
